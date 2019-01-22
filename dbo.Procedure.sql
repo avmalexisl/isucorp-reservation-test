8877e0120a144b34e5697087c4e6e94e63938b37
Alejandro Vázquez Menéndez <avmalexisl@gmail.com>
2019-01-22 19:27:49 +0100

    Stored Procedure SQL File
    Need to add this .sql to the DB Stored Procedures

---
+++ dbo.Procedure.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetContact]
+	@Id int = 0
+AS
+	SELECT Id,
+           Name,
+           Phone,
+           BirthDate,
+           Type,
+           Description FROM Contacts
+	WHERE Contacts.Id = @Id
